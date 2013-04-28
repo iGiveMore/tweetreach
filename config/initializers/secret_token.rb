18494a7163cea5a85eb972184a6134e2771299cb
Wylie Conlon <wylieconlon@gmail.com>
2013-04-27 20:45:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetreach::Application.config.secret_token = 'd0c4265664184358277d356dcf4caa6301a36d29f5c904e9b4794c3b5e79101f97d91c89f23bb5738e47d18894f2ad2d83ef2277a7c2ce1aafccba85b944e7f1'
