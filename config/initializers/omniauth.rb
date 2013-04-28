bb8b44b184efcbb286c6f0f5d2ce42a31773f8fa
AHMAD <ahmadalokush@AHMADs-MacBook-Pro.local>
2013-04-28 01:02:21 -0400

    added small config to test for dev environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'H5moFHvICy2fDAnv5Gg', '7P1BI4Bpl0r1FUYYZxGd5uW78TigeZ2fr4ttveLpA'
+   if Rails.env.development?
+      OmniAuth.config.full_host = "http://localhost:3000/auth/twitter"
+   end
+   
 end
