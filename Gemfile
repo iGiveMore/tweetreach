79a0bbd6720e74e8a7fdaf38d740404d666361a1
Wylie Conlon <wylieconlon@gmail.com>
2013-04-28 04:55:16 -0400

    Connect twitter gem after authentication for api calls

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'jruby-openssl'
 
 gem 'puma'
 
+gem 'twitter'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < Neo4j::Rails::Model
   property :status_count, :type => String
   property :friends_count, :type => String
   property :followers_count, :type => String
+  property :oauth_token, :type => String
+  property :oauth_secret, :type => String
+
+  has_n(:follows).to(User)
 
   def self.find_or_create_from_auth_hash(hash)
     userinfo = hash["extra"]["raw_info"]
@@ -28,9 +32,53 @@ class User < Neo4j::Rails::Model
       user.status_count = userinfo['status_count']
       user.friends_count = userinfo['friends_count']
       user.followers_count = userinfo['followers_count']
+      user.oauth_token = hash["credentials"]["token"]
+      user.oauth_secret = hash["credentials"]["secret"]
 
       user.save
+
+      client = erik = Twitter::Client.new(
+        :oauth_token => user.oauth_token,
+        :oauth_token_secret => user.oauth_secret
+      )
+
+      # friends = Twitter.friends
+      # followers = Twitter.followers
+
+      # friends.each do |account|
+      #   puts account.screen_name
+
+      #   u = User.find_or_create_from_twitter(account)
+
+      #   user.outgoing(:follows) << u
+
+      #   break
+      # end
     end
+
+    return user
+  end
+
+  def self.find_or_create_from_twitter(account)
+    user = User.find('screen_name: %s' % account.screen_name)
+
+    if !user
+      user = User.new
+
+      # user.user_id = account.id
+      user.screen_name = account.screen_name
+      user.name = account.name
+    end
+
+    user.profile_image_url = account.profile_image_url
+    user.location = account.location
+    user.url = account.url
+    user.description = account.description
+    user.status_count = account.status_count
+    user.friends_count = account.friends_count
+    user.followers_count = account.followers_count
+
+    user.save
 
     return user
   end
