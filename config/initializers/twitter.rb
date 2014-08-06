1058e129d1d2413c6fb561d86bfb5688e09d52e3
Matt Bishop <Matt@igivemore.org>
2014-08-06 11:55:13 -0400

    Delete twitter.rb

--- config/initializers/twitter.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = "H5moFHvICy2fDAnv5Gg"
-  config.consumer_secret = "7P1BI4Bpl0r1FUYYZxGd5uW78TigeZ2fr4ttveLpA"
-  config.oauth_token = "7028552-aCKfaoIF1BlAClVSPhfuyMgeD4nVHPizctYJbp86QQ"
-  config.oauth_token_secret = "IhpVtGPNQl4v6LSsvnw26hexKj8OJNz52dB1UlB4"
-end
